dce0b1c67cf7adcb768d2f290738b2020ed80dab
Waiyan Htet <waiyanhtet@DMM-Mac1430GJDV.local>
2024-09-18 11:29:30 +0630

    added: cloud config server project

---
+++ src/main/java/dev/waiyanhtet/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,23 @@
+package dev.waiyanhtet.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/dev/waiyanhtet/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.waiyanhtet.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
